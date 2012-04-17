5b89e15b7bd527ef09ec081c786a017d1cb5dff1
Tom Mays <contact@tommays.net>
2012-04-17 10:12:04 +0200

    loadproperties.js : changed order of "os" and "osversion" so that "os" is first.

--- library/js/loadproperties.js
+++ library/js/loadproperties.js
@@ -9,8 +9,8 @@ setoutletassist(0,"patcher filepath and more");
 function loadbang()
 {
 	outlet(0, "isruntime",max.isruntime);
-	outlet(0, "osversion",max.osversion);
 	outlet(0, "os",max.os);
+	outlet(0, "osversion",max.osversion);
 	outlet(0, "maxversion",max.version);
 	outlet(0, "name",patcher.name);	
 	outlet(0, "filepath",patcher.filepath)
@@ -18,8 +18,8 @@ function loadbang()
 function bang()
 {
 	outlet(0, "isruntime",max.isruntime);
-	outlet(0, "osversion",max.osversion);
 	outlet(0, "os",max.os);
+	outlet(0, "osversion",max.osversion);
 	outlet(0, "maxversion",max.version);
 	outlet(0, "name",patcher.name);	
 	outlet(0, "filepath",patcher.filepath)
