7c31de545521591ae5673251f339bec4507e40bd
reno- <contact@rubiano.fr>
2014-01-26 16:39:04 +0100

    implement a way to know which version of span we are running

--- max-package/javascript/jsproperties.js
+++ max-package/javascript/jsproperties.js
@@ -6,14 +6,22 @@
 outlets = 1;
 setoutletassist(0,"patcher filepath and more");
 
+// We need to found an automatic way to update this variable when pushing to the git repo
+SPAN_VERSION = "0.0.1";
+SPAN_REV = "4qd4cd";
+
+
 function bang()
 {
+	outlet(0, "isplugin", max.isplugin);
 	outlet(0, "isruntime",max.isruntime);
-	outlet(0, "arch",max.arch);
 	outlet(0, "os",max.os);
 	outlet(0, "osversion",max.osversion);
 	outlet(0, "maxversion",max.version);
+	outlet(0, "span_version", SPAN_VERSION);
+	outlet(0, "span_revision", SPAN_REV);
 	outlet(0, "name",patcher.name);	
 	outlet(0, "filepath",patcher.filepath)
 	outlet(0, "apppath",max.apppath + "/")
 }
+
